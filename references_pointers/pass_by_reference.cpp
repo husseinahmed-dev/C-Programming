897db41dddf5dd66f80bc4c908e2d353a79108f3
Hussein Ahmed Shahab <hussein.a.shahab@gmail.com>
2023-01-10 16:29:49 +0300

    Added pass_by_reference

---
+++ references_pointers/pass_by_reference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int triple(int i) {
+	i = i * 3;
+	return i;
+}
+
+int main() {
+	int num = 1;
+
+	std::cout << triple(num) << "\n";	// Pass-by-Value: 3, Pass-by-Reference: 3
+	std::cout << triple(num) << "\n";	// Pass-by-Value: 3, Pass-by-Reference: 9
+}
