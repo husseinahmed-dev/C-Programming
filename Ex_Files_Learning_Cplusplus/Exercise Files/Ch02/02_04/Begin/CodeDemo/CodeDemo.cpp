6085982367cbe3af23bc1a92dbfb77ade9f7ecc8
Hussein Ahmed Shahab <hussein.a.shahab@gmail.com>
2023-01-26 00:48:45 +0300

    Updated .gitignore

--- Ex_Files_Learning_Cplusplus/Exercise Files/Ch02/02_04/Begin/CodeDemo/CodeDemo.cpp
+++ Ex_Files_Learning_Cplusplus/Exercise Files/Ch02/02_04/Begin/CodeDemo/CodeDemo.cpp
@@ -8,5 +8,15 @@
 using namespace std;
 
 int main(){
+    auto a = 8;
+    auto b = 3.2f;
+    auto c = 'a';
+    auto d = 3.14;
+    auto e = "hussein";
+    cout << typeid(a).name() << endl;
+    cout << typeid(b).name() << endl;
+    cout << typeid(c).name() << endl;
+    cout << typeid(d).name() << endl;
+    cout << typeid(e).name() << endl;
     return (0);
 }
